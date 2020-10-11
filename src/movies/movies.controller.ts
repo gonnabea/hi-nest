90c1fe977f459f3bf647247dea62202094e88b7b
Choi Jiwon <winning1993@naver.com>
2020-10-11 21:23:49 +0900

    #2.1 More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
+
     @Get()
     getAll() {
         return "모든 영화 데이터를 반환할 것."
     }
+
+    @Get("search")
+    search(@Query("year") searchByYear:string){
+        return `영화 데이터 검색 (year:${searchByYear})`
+    }
+
     @Get("/:id")
     getOne(@Param("id") movieId:string){
         return `특정 영화 데이터를 반환 (id:${movieId})`
     }
 
-    @Post("/:id")
-    create(@Param("id") movieId:string){
-        return `영화 데이터를 생성 (id:${movieId})`
+    @Post()
+    create(@Body() movieData){
+        console.log(movieData)
+        return movieData
     }
 
     @Delete("/:id")
@@ -22,8 +30,11 @@ export class MoviesController {
     }
 
     @Patch("/:id")
-    patch(@Param("id") movieId:string) {
-        return `특정 영화 데이터를 패치(업데이트) (id:${movieId})`
+    patch(@Param("id") movieId:string, @Body() updateData) {
+        return {
+            updatedMovie: movieId,
+            ...updateData,
+        }
     }
 
 }
