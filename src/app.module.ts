4214e75823198c1b9ddd4ae4ccf39449afc39153
Choi Jiwon <winning1993@naver.com>
2020-10-25 05:15:45 +0900

    #2.6 Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
+  imports: [MoviesModule],
+  controllers: [AppController],
   providers: [MoviesService],
 })
 export class AppModule {}
