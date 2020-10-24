9ecca1692eeef516577716dfc2d10e0dc77d212b
Choi Jiwon <winning1993@naver.com>
2020-10-24 23:59:38 +0900

    #2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,32 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
-@Controller('movies')
+@Controller('movies') // 기본 주소
 export class MoviesController {
 
-    @Get()
-    getAll() {
-        return "모든 영화 데이터를 반환할 것."
+    constructor(private readonly moviesService: MoviesService) {
+
     }
 
-    @Get("search")
-    search(@Query("year") searchByYear:string){
-        return `영화 데이터 검색 (year:${searchByYear})`
+    @Get()
+    getAll(): Movie[]{
+        return this.moviesService.getAll()
     }
+
+    // @Get("search")
+    // search(@Query("year") searchByYear:string){
+    //     return `영화 데이터 검색 (year:${searchByYear})`
+    // }
 
     @Get("/:id")
-    getOne(@Param("id") movieId:string){
-        return `특정 영화 데이터를 반환 (id:${movieId})`
+    getOne(@Param("id") movieId:string): Movie{
+        return this.moviesService.getOne(movieId)
     }
 
     @Post()
     create(@Body() movieData){
-        console.log(movieData)
-        return movieData
+       return this.moviesService.create(movieData)
     }
 
     @Delete("/:id")
     remove(@Param("id") movieId:string) {
-        return `영화 데이터를 삭제 (id:${movieId})`
+        return this.moviesService.deleteOne(movieId)
     }
 
     @Patch("/:id")
